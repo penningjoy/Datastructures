5376019a53d4110f30d9a336ee82c62572ebed54
Joydeep Banerjee <joydeepbanerjee2@outlook.com>
2020-04-23 23:09:28 -0400

    second commit

--- stack.py
+++ stack.py
@@ -1,28 +1,31 @@
-# Declare a stack using a list
-namesstack = []
+def stackOperations():
+    # Declare a stack using a list
+    namesstack = []
 
-namesstack.append('Joydeep')
-namesstack.append('Pushpita')
-namesstack.append('Ashis')
-namesstack.append('Debopriya')
+    namesstack.append('Joydeep')
+    namesstack.append('Pushpita')
+    namesstack.append('Ashis')
+    namesstack.append('Debopriya')
 
-print('..................')
-print('Stack Initially...')
-print(namesstack)
-print('..................')
+    print('..................')
+    print('Stack Initially...')
+    print(namesstack)
+    print('..................')
 
-print('First pop: ' + namesstack.pop())
-print('Second pop: ' + namesstack.pop())
-print('Third pop: ' + namesstack.pop())
+    print('First pop: ' + namesstack.pop())
+    print('Second pop: ' + namesstack.pop())
+    print('Third pop: ' + namesstack.pop())
 
-print('Adding a few more names..')
+    print('Adding a few more names..')
 
-namesstack.append('Joydeep')
-namesstack.append('Sourajit')
-namesstack.append('Shanaya')
+    namesstack.append('Joydeep')
+    namesstack.append('Sourajit')
+    namesstack.append('Shanaya')
 
-print('..................')
-print('Stack Now....')
-print(namesstack)
-print('..................')
+    print('..................')
+    print('Stack Now....')
+    print(namesstack)
+    print('..................')
 
+#  ---- Call the function ---
+stackOperations()
