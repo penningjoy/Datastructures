ebf17733a272422b422f973611297d50b5fa9a8c
Joydeep Banerjee <joydeepbanerjee2@outlook.com>
2020-04-23 23:05:39 -0400

    first commit

---
+++ stack.py
@@ -0,0 +1,28 @@
+# Declare a stack using a list
+namesstack = []
+
+namesstack.append('Joydeep')
+namesstack.append('Pushpita')
+namesstack.append('Ashis')
+namesstack.append('Debopriya')
+
+print('..................')
+print('Stack Initially...')
+print(namesstack)
+print('..................')
+
+print('First pop: ' + namesstack.pop())
+print('Second pop: ' + namesstack.pop())
+print('Third pop: ' + namesstack.pop())
+
+print('Adding a few more names..')
+
+namesstack.append('Joydeep')
+namesstack.append('Sourajit')
+namesstack.append('Shanaya')
+
+print('..................')
+print('Stack Now....')
+print(namesstack)
+print('..................')
+
